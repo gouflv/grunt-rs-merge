e2bf8135b7dcd95c101148469543aaa47d1c54ba
gouflv <lv.gouf@gmail.com>
2013-06-03 17:39:37 +0800

    update testing

--- test/expected/main.js
+++ test/expected/main.js
@@ -1,6 +1,6 @@
 (function() {
 	
-	var tmpl = '<h1>Testing</h1><span>ss</span><div class="classname">中文</div>';
+	var tmpl = '<h1>Testing</h1><span>ss</span><div class="classname" style="display:none; 	background: #111">中文</div>';
 
 	var style = '*{font-size:11px}body{font-family:\'宋体\';background:#ccc;_backgrund:#fff}';
 
